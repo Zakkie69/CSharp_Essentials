7093f23ae13236b6e2ac2b1d84e2cb381e4934d3
Zakkie69 <regirastalol@gmail.com>
2022-09-18 19:30:30 +0200

    Instructies nieuw csharp bestand

--- Testproject/Program.cs
+++ Testproject/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//Eerst naar directory gaan
+//Daarna dotnet new + short name 
+
+//dotnet run om uit te voeren
