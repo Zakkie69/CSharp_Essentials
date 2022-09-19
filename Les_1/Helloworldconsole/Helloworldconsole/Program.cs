b78700973bd5dbd9b7790c9d9a791cabf0804f5b
Robbe van Hoppe <12200496@student.pxl.be>
2022-09-19 11:57:07 +0200

    opgeruimd

---
+++ Les_1/Helloworldconsole/Helloworldconsole/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Geef naam: ");
+        string Naam = Console.ReadLine();
+        Console.WriteLine("Voornaam: " + Naam.Split(' ')[0]);
+        Console.Write("Achternaam: " + Naam.Split(' ')[1]+ " ");
+
+        if (Naam.Split(' ')[2] != "")
+            Console.WriteLine(Naam.Split(' ')[2]);
+        
+    }
+}
