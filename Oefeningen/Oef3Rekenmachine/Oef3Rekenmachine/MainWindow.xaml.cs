8a7da9e15689e86087ebdb36c229bfa394b497f9
Robbe van Hoppe <12200496@student.pxl.be>
2022-09-19 15:18:30 +0200

    Oefening rekenmachine semi klaar

--- Oefeningen/Oef3Rekenmachine/Oef3Rekenmachine/MainWindow.xaml.cs
+++ Oefeningen/Oef3Rekenmachine/Oef3Rekenmachine/MainWindow.xaml.cs
@@ -20,9 +20,68 @@ namespace Oef3Rekenmachine
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
+
             InitializeComponent();
+            
+
+    }
+
+
+        public Decimal Getal2;
+        public Decimal Getal1;
+        
+
+        public void KrijgGetallen() 
+        {
+            //publieke functie om de getallen te krijgen
+
+            Decimal Getal1 = Convert.ToDecimal(TxtGetal1.Text);
+            Decimal Getal2 = Convert.ToDecimal(TxtGetal2.Text);
+            
+
+        }
+
+        public void BtnMin_Click(object sender, RoutedEventArgs e)
+        {
+            KrijgGetallen();
+
+            Decimal resultaat = Getal1 - Getal2;
+            TxtResultaat.Text = Convert.ToString(resultaat);
+        }
+
+        public void BtnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            KrijgGetallen();
+
+            Decimal resultaat = Getal1 + Getal2;
+            TxtResultaat.Text = Convert.ToString(resultaat);
+        }
+
+        private void BtnDeel_Click(object sender, RoutedEventArgs e)
+        {
+            KrijgGetallen();
+
+            Decimal resultaat = Getal1 / Getal2;
+            TxtResultaat.Text = Convert.ToString(resultaat);
+        }
+
+        private void BtnMaal_Click(object sender, RoutedEventArgs e)
+        {
+            KrijgGetallen();
+
+            Decimal resultaat = Getal1 * Getal2;
+            TxtResultaat.Text = Convert.ToString(resultaat);
+        }
+
+        private void BtnWis_Click(object sender, RoutedEventArgs e)
+        {
+            TxtGetal1.Text = "0";
+            TxtGetal2.Text = "0";
+            TxtResultaat.Text = "0";
         }
     }
 }
