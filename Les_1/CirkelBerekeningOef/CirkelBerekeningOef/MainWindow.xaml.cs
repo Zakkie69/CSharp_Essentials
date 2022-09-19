d3a5fc37fd2e44921b9744d61fa67aa9c05ee5b6
Robbe van Hoppe <12200496@student.pxl.be>
2022-09-19 14:26:38 +0200

    Cirkel oefening klaar

---
+++ Les_1/CirkelBerekeningOef/CirkelBerekeningOef/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CirkelBerekeningOef
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBereken_Click(object sender, RoutedEventArgs e)
+        {
+            Double Straal = Convert.ToDouble(TxtStraal.Text);
+            Double Omtrek = Straal*2*Math.PI;
+            Double Oppervlakte = Math.Pow(Straal,2) * Math.PI;
+
+            TxtOppervlakte.Text = Oppervlakte.ToString();
+            TxtOmtrek.Text = Omtrek.ToString();
+        }
+
+        private void BtnWis_Click(object sender, RoutedEventArgs e)
+        {
+            TxtOmtrek.Text = "0";
+            TxtOppervlakte.Text = "0";
+            TxtStraal.Text = "0";
+        }
+    }
+}
