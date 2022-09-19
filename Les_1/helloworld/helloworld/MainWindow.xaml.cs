e52ed7754ffd36bbb355522e66a1865ffb625717
Robbe van Hoppe <12200496@student.pxl.be>
2022-09-19 11:54:28 +0200

    wpf programma dood

---
+++ Les_1/helloworld/helloworld/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace helloworld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Knop_Click(object sender, RoutedEventArgs e)
+        {
+
+            // Knop.Content = "Hello world";
+
+            for (int i = 0; i < 99; i++)
+            {
+                i /= 100;
+                leebel.Content = i;
+            }
+
+
+            
+
+
+        }
+    }
+}
