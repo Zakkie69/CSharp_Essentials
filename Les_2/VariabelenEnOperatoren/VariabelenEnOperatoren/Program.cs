a39da997caa480ccafbc6e3b57e336df2e60e9e4
Robbe van Hoppe <12200496@student.pxl.be>
2022-09-20 12:17:52 +0200

    Les 2 gedaan

---
+++ Les_2/VariabelenEnOperatoren/VariabelenEnOperatoren/Program.cs
@@ -0,0 +1,33 @@
+﻿
+//  Declaratie:
+//  Datatype Naam
+
+double kommagetal;
+float kleinerKommagetal;
+decimal groterKommagetal;
+
+string tekst;
+
+//  ja nee is het antwoord, dus best een vraag in de variabele
+bool isRood = true;
+bool heeftWielen = false;
+
+//  Minder goede naamgeving
+
+bool a;
+
+//  Initialisatie
+//  naam = value
+
+kleinerKommagetal = 0.1f;
+kommagetal = 0.01;
+groterKommagetal = 0.001m;
+
+int geheelGetal = 10;
+short gleinGetal = 1;
+long grootGetal = 100;
+
+//  mudulo, je schrijft de rest naar de var
+geheelGetal = 25 % 12;
+
+Console.WriteLine(geheelGetal);
